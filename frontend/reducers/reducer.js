9dd3f73a884142730558292a7b5668e943c14179
Alex Greenberg <algreenberg89@gmail.com>
2024-08-03 17:21:35 -0400

    redux-boilerplate 1.0

---
+++ frontend/reducers/reducer.js
@@ -0,0 +1,13 @@
+import * as types from '../constants/actionsTypes.js';
+
+const initialState = {
+  location: '',
+  user: '',
+};
+
+const apiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SET_LOCATION: {
+    }
+  }
+};
