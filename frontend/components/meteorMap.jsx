4905ee3a20c6c44cb0eb8ab7efe6e8f5df2ebffe
Alex Greenberg <algreenberg89@gmail.com>
2024-08-05 15:40:14 -0400

    working on meteormap menu

--- frontend/components/meteorMap.jsx
+++ frontend/components/meteorMap.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 
 const MeteorMap = (props) => {
-  const fetchedMap = () => {};
+  const [meteor, setMeteor] = useState('');
+
+  const handleSelect = async (val) => {
+    fetch('http:www.localhost:3000', {
+      'Content-Type': 'application/json',
+    });
+  };
+
   return (
     <div className="meteormap">
       <h3>MeteorMap</h3>
+      <label>Choose Meteor Shower</label>
+      <select
+        name="meteor-shower-menu"
+        onSelect={(e) => {
+          setMeteor(e.target.value);
+          handleSelect(meteor);
+        }}
+      >
+        <option value="geminids">Geminids</option>
+        <option value="perseids">Pereids</option>
+        <option value="quadrantids">Quadrantids</option>
+        <option value="eta_aquariids">Eta Aquariids</option>
+      </select>
     </div>
   );
 };
