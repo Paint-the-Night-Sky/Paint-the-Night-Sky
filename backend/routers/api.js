1f5c94f625747e1fa58727ea310974d6b67a73e3
aaron <aaronthien052401@gmail.com>
2024-08-04 23:18:26 -0400

    added another route and fetch reques to api

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -7,6 +7,10 @@ router.post('/starChart', apiController.starChartData, (req, res)=> {
     res.status(200).json(res.locals.starChart);
 } )
 
+router.post('moonData'), apiController.meteorChartData, (req, res) => {
+    res.status(200).json(res.locals.moonData);
+}
+
 
 /* 
 
