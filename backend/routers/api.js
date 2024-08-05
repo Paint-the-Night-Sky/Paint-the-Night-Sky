8efafa591b82240cb31b8b7b58072690529a0bd6
aaron <aaronthien052401@gmail.com>
2024-08-04 23:19:34 -0400

    Revert "added another route and fetch reques to api"
    This reverts commit 1f5c94f625747e1fa58727ea310974d6b67a73e3.

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -7,10 +7,6 @@ router.post('/starChart', apiController.starChartData, (req, res)=> {
     res.status(200).json(res.locals.starChart);
 } )
 
-router.post('moonData'), apiController.meteorChartData, (req, res) => {
-    res.status(200).json(res.locals.moonData);
-}
-
 
 /* 
 
