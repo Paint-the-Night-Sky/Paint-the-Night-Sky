80f9b9970c332c0366b686ec73f2e70325959636
Ali McNamara <allisonrm95@gmail.com>
2024-08-05 15:09:07 -0400

    updates to eventsData controller

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -162,8 +162,11 @@ const apiController = {
       let response = await fetch(
         `https://api.astronomyapi.com/api/v2/bodies/events/sun?latitude=${lat}&longitude=${long}&elevation=100&from_date=${currentDate}&to_date=${monthAway}&time=12:00:00&output=table`
       );
-
-      res.locals.eventsData = response.json();
+      const responseInfo = [
+        response.data.rows[0].body,
+        response.data.rows[0].events,
+      ];
+      res.locals.eventsData = responseInfo.json();
       return next();
     } catch (error) {
       return next({
@@ -175,5 +178,55 @@ const apiController = {
   },
 };
 module.exports = apiController;
-
+/*
+{
+    "data": {
+        "dates": {
+            "from": "2020-12-20T09:00:00.000-05:00",
+            "to": "2021-12-23T09:00:00.000-05:00"
+        },
+        "observer": {
+            "location": {
+                "longitude": -84.39733,
+                "latitude": 38.775867,
+                "elevation": 0
+            }
+        },
+        "rows": [
+            {
+                "body": {
+                    "id": "sun",
+                    "name": "Sun"
+                },
+                "events": [
+                    {
+                        "type": "partial_solar_eclipse",
+                        "eventHighlights": {
+                            "partialStart": {
+                                "date": "2021-06-10T03:45:15.241-05:00",
+                                "altitude": -13.56
+                            },
+                            "totalStart": null,
+                            "peak": {
+                                "date": "2021-06-10T04:37:30.267-05:00",
+                                "altitude": -5.85
+                            },
+                            "totalEnd": null,
+                            "patialEnd": {
+                                "date": "2021-06-10T05:32:52.677-05:00",
+                                "altitude": 2.92
+                            }
+                        },
+                        "rise": "2021-06-10T10:12:14.143Z",
+                        "set": "2021-06-11T01:02:13.766Z",
+                        "extraInfo": {
+                            "obscuration": 0.76
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
+    */
 // https://api.meteomatics.com/2024-08-05T11:15:00.000-04:00/t_2m:C/40.730925,-73.9355578/json?model=mix
